95172c923d17be609848ea4a340b2edd5dcc61fe
Darrell Tisdale <Darrell.Tisdale@outlook.com>
2021-11-07 21:12:12 -0500

    added alert to show if the pin is correct or not

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 "use strict";
 const prompt = require("prompt-sync")();
-const  getBalance  = require("./atm")
-const  withdraw  = require("./atm")
-const deposit  = require("./atm")
-const  validatePin  = require("./atm")
+let  getBalance  = require("./atm")
+let  withdraw  = require("./atm")
+let deposit  = require("./atm")
+let  validatePin  = require("./atm")
 //TODO: Add debugger (see demo video)
 //! Don't forget to add "console": "integratedTerminal" to .vscode/launch.json after creating launch configuration
 //TODO: Import necessary functions from atm.js
 //TODO: Utilize prompt-sync so we can get user input for various functions
 //* Refer to Intro to Node.js PowerPoint for prompt-sync installation instructions
-mainMenu();
+
+
+
+
 function accessATM(currentAtmPin) {
   //TODO: Prompt users for their pin
   let userInput = prompt("Please Enter Pin")
-  if(userInput!=currentAtmPin){
-    userInput=prompt("Please Enter Correct Pin")
+  if(userInput===currentAtmPin){
+    mainMenu();
   }
+  else if (userInput!=currentAtmPin){
+    userInput=prompt("Please Enter Correct Pin")
+    if (userInput!=currentAtmPin){
+      userInput=prompt("Acces Denied")
+    }
+  }
+  
   //Useches
   //Proceed to main menu ONLY if they match
 }
-
+accessATM(validatePin)
 //TODO: Call accessATM function
 
 function mainMenu() {
